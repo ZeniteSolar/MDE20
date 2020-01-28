0fba8646c0855cd6f67d10c831d35a718069b1e4
João Antônio Cardoso <joao.maker@gmail.com>
2020-01-28 12:02:25 -0300

    small update

--- firmware/up.sh
+++ firmware/up.sh
@@ -2,6 +2,6 @@
 make clean
 rm -rf bin obj
 make all
-#fuser -k /dev/ttyACM0
-avrdude -c arduino -P /dev/ttyACM0 -p m328p -D -U flash:w:bin/Firmware.elf
-figlet "MIC19"
+#fuser -k /dev/ttyUSB0
+avrdude -c arduino -P /dev/ttyUSB0 -p m328p -D -U flash:w:bin/firmware.elf
+figlet "MDE20"
